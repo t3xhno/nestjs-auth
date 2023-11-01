1d53ac15018c6446316250636725cc4da4a04d79
tex <texhnolainx@gmail.com>
2023-11-02 00:53:19 +0100

    removed unused bookmarks module for now

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
   imports: [
     AuthModule,
     UserModule,
     PrismaModule,
-    BookmarkModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
 })
